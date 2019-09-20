187841890134d79120c311bb834955da636f969c
NateShetler <nate.shetler@gmail.com>
2019-09-20 09:13:01 -0400

    Refactored creating the graph

--- DataStructures-Project1/components.cpp
+++ DataStructures-Project1/components.cpp
@@ -5,6 +5,49 @@
 #include <iterator>
 #include <iomanip>
 
+// This function will accept an ifstream and graph
+// This function will create the graph in the correct format
+void createGraph(std::ifstream &input, Vector <List<int>>& graph)
+{
+	// For testing purposes
+	std::string inputString = "";
+	int numInFile = 0;
+	int vecInt = 0;
+
+	bool endLine = false;
+
+	while (!input.eof())
+	{
+		// Create new list and push back the list onto the graph
+		List<int> newList;
+		graph.push_back(newList);
+
+		while (!endLine)
+		{
+			input >> inputString;
+			std::cout << inputString << " ";
+
+			numInFile = std::stoi(inputString);
+			graph[vecInt].push_back(numInFile);
+
+			if (input.peek() == '\n' || input.eof())
+			{
+				endLine = true;
+			}
+		}
+
+		std::cout << std::endl;
+
+		// Add one to the vector integer (what keeps track of the 
+		// position in the vector)
+		++vecInt;
+
+		// Reset the boolean
+		endLine = false;
+
+		//std::cout << fileName << std::endl;
+	}
+}
 // This function will output the graph data structure
 void printGraph(Vector <List<int>>& graph)
 {
@@ -44,44 +87,8 @@ int main()
 		input.open(fileName.c_str());
 	}
 
-	// For testing purposes
-	std::string inputString = "";
-	int numInFile = 0;
-	int vecInt = 0;
-
-	bool endLine = false;
-
-	while (!input.eof())
-	{
-		// Create new list and push back the list onto the graph
-		List<int> newList;
-		graph.push_back(newList);
-
-		while (!endLine)
-		{
-			input >> inputString;
-			std::cout << inputString << " ";
-
-			numInFile = std::stoi(inputString);
-			graph[vecInt].push_back(numInFile);
-
-			if (input.peek() == '\n' || input.eof())
-			{
-				endLine = true;
-			}
-		}
-
-		std::cout << std::endl;
-
-		// Add one to the vector integer (what keeps track of the 
-		// position in the vector)
-		++vecInt;
-
-		// Reset the boolean
-		endLine = false; 
-
-		//std::cout << fileName << std::endl;
-	}
+	// Create the graph
+	createGraph(input, graph);
 
 	// Print the graph
 	printGraph(graph);
