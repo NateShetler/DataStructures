d86b9830eed2b61c5b7ecf663e5adb8c3f30525c
NateShetler <nate.shetler@gmail.com>
2019-10-18 10:15:01 -0400

    Finished project3

--- DataStructures-Project3/empdriver.cpp
+++ DataStructures-Project3/empdriver.cpp
@@ -1,11 +1,22 @@
-#include "empmaps.cpp"
+#include "empmaps.h"
 
 int main()
 {
+	// Create the employee vector
 	std::vector<Employee> empVec = employees();
 
+	// Create the two different maps based on department and salary
 	std::map<int, std::vector<Employee>> deptMap = mapEmpDept(empVec);
 	std::map<int, std::vector<Employee>> salMap = mapSalRange(empVec);
+
+	// Print the salary map
 	printSalRange(salMap);
+
+	// Create teh two different unordered maps based on department and salary
+	std::unordered_map<int, std::vector<Employee>> deptUnMap = umapEmpDept(empVec);
+	std::unordered_map<int, std::vector<Employee>> salUnMap = umapSalRange(empVec);
+
+	// Print the salary unordered map
+	uprintSalRange(salUnMap);
 	
 }
--- DataStructures-Project3/empmaps.h
+++ DataStructures-Project3/empmaps.h
@@ -1,7 +1,7 @@
-#ifndef EMPMAPS_H_INCLUDING
-#define EMPMAPS_H_INCLUDING
+#ifndef EMPMAPS_H_INCLUDED
+#define EMPMAPS_H_INCLUDED
 
-#include "Employee.cpp"
+#include "Employee.h"
 #include <vector>
 #include <map>
 #include <unordered_map>
@@ -13,9 +13,9 @@ std::vector<Employee> employees();
 std::map<int, std::vector<Employee>> mapEmpDept(std::vector<Employee>& emp);
 std::map<int, std::vector<Employee>> mapSalRange(std::vector<Employee>& emp);
 void printSalRange(std::map<int, std::vector<Employee>>& salRange);
-/*
+
 std::unordered_map<int, std::vector<Employee>> umapEmpDept(std::vector<Employee>& emp);
 std::unordered_map<int, std::vector<Employee>> umapSalRange(std::vector<Employee>& emp);
-void uprintSalRange(std::unordered_map<int, std::vector<Employee>>& salRange);*/
+void uprintSalRange(std::unordered_map<int, std::vector<Employee>>& salRange);
 
-#endif // EMPSMAPS_H_INCLUDING 
+#endif // EMPSMAPS_H_INCLUDED
